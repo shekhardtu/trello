9d273ecce485fc053f23ec61deb2feaf985b7518
Hari Shekhar <hari.shekhar@landmarkgroup.in>
2019-09-17 08:34:10 +0530

    removed app.configuration'

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ var path = require("path");
 
 app = express();
 
-app.configure(function() {
-  app.use(express.bodyParser());
-  app.use(express.static(path.join(__dirname, "dist")));
-});
+app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, "dist")));
 
 var port = process.env.PORT || 5000;
 
