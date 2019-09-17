b773616853595aedb8a6b259cde6ba52a2f8370e
Hari Shekhar <hari.shekhar@landmarkgroup.in>
2019-09-17 08:51:47 +0530

    node file added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require("express");
 
 var path = require("path");
+var bodyParser = require("body-parser");
 
 app = express();
 
-app.use(express.bodyParser());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static(path.join(__dirname, "dist")));
 
 var port = process.env.PORT || 5000;
